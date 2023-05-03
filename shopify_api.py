97e9be5f0960c17986eb25ad511aa518389fd06b
jacksonmilburn <jacksonemilburn@gmail.com>
2023-05-03 02:32:47 -0500

    Essential updates

---
+++ shopify_api.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""shopify_api.py wrapper script for running it the source directory"""
+
+import sys
+import os.path
+
+# Use the development rather than installed version
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
+
+with open(os.path.join(project_root, "scripts", "shopify_api.py")) as f:
+    code = compile(f.read(), f.name, "exec")
+    exec(code)
