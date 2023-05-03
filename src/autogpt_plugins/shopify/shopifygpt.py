d2146fdd58b82eabe48854e41b3a9458d8e6a68e
jacksonmilburn <jacksonemilburn@gmail.com>
2023-05-03 07:03:12 -0500

    Update shopifygpt.py

--- src/autogpt_plugins/shopify/shopifygpt.py
+++ src/autogpt_plugins/shopify/shopifygpt.py
@@ -9,6 +9,13 @@ import functools
 import yaml
 import six
 from six.moves import input, map
+from shopify.version import VERSION
+from shopify.session import Session, ValidationException
+from shopify.resources import *
+from shopify.limits import Limits
+from shopify.api_version import *
+from shopify.api_access import *
+from shopify.collection import PaginatedIterator
 import requests
 
 
