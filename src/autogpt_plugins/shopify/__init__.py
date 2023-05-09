c03f2fe745224436bbd8fdfbb841d4f8649ed697
jacksonmilburn <jacksonemilburn@gmail.com>
2023-05-09 01:04:53 -0500

    Folder Change

--- src/autogpt_plugins/shopify/__init__.py
+++ src/autogpt_plugins/shopify/__init__.py
@@ -4,13 +4,6 @@ import os.path
 import shopify
 from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict
 from auto_gpt_plugin_template import AutoGPTPluginTemplate
-from shopify.version import VERSION
-from shopify.session import Session, ValidationException
-from shopify.resources import *
-from shopify.limits import Limits
-from shopify.api_version import *
-from shopify.api_access import *
-from shopify.collection import PaginatedIterator
 
 
 PromptGenerator = TypeVar("PromptGenerator")
