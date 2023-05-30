ae86c3a3d7772dd0cde4def9f70253db55aea05e
jacksonmilburn <jacksonemilburn@gmail.com>
2023-05-30 16:36:04 -0500

    Update __init__.py

--- src/autogpt_plugins/shopify_gpt_api/__init__.py
+++ src/autogpt_plugins/shopify_gpt_api/__init__.py
@@ -432,7 +432,7 @@ class ShopifyAutoGPT(AutoGPTPluginTemplate):
             "create_collection",
             {
                 "title": "<title>",
-                "collection_type": "<collection_type>"
+                "collection_type":  {"choices": ["custom", "smart"]}
             },
             create_collection,
         )
