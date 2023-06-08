5ce3a31729f7439b6add752218801cc24207f023
jacksonmilburn <jacksonemilburn@gmail.com>
2023-06-07 23:02:05 -0500

    Update __init__.py

--- src/autogpt_plugins/shopify_gpt_api/__init__.py
+++ src/autogpt_plugins/shopify_gpt_api/__init__.py
@@ -77,7 +77,6 @@ class ShopifyAutoGPT(AutoGPTPluginTemplate):
             try:
                 credentials = {
                     "developer_token": self.developer_token,
-                    "access_token": self.access_token,
                     "refresh_token": self.refresh_token,
                     "client_id": self.client_id,
                     "client_secret": self.client_secret,
