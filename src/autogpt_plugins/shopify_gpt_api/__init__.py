2a7379b8676164e14346b7f64a521195ebd03a3f
jacksonmilburn <jacksonemilburn@gmail.com>
2023-06-07 19:15:46 -0500

    Update __init__.py

--- src/autogpt_plugins/shopify_gpt_api/__init__.py
+++ src/autogpt_plugins/shopify_gpt_api/__init__.py
@@ -76,13 +76,13 @@ class ShopifyAutoGPT(AutoGPTPluginTemplate):
         ) is not None:
             print('Authenticating to Google Ads...')
             try:
-                credentials = Credentials.from_authorized_user_info({
-                    'client_id': self.client_id,
-                    'client_secret': self.client_secret,
-                    'refresh_token': self.refresh_token,
-                    'token': self.access_token,
-                    'token_uri': 'https://www.googleapis.com/oauth2/v4/token',
-                })
+                credentials = Credentials(
+                    token=self.access_token,
+                    refresh_token=self.refresh_token,
+                    token_uri='https://www.googleapis.com/oauth2/v4/token',
+                    client_id=self.client_id,
+                    client_secret=self.client_secret,
+                )
 
                 self.googleads_client = GoogleAdsClient(credentials=credentials, developer_token=self.developer_token, version="v13")
 
